7c583d4ac6e11f48bac2a04f575c0671729ce9b5
CuteCubed <android11089@gmail.com>
2018-12-08 11:54:59 +0100

    Possible Security issue Log removed

--- dashboard/js/page_about.js
+++ dashboard/js/page_about.js
@@ -1,14 +1,19 @@
-$( "#address" ).val( address );
-$( "#public" ).val( publickey );
 
 $( "#generate-button" ).click( function () {
   $( "#private" ).val( privatekey );
 } );
 
 
+
+
 $( "#generate-qr-button" ).click( function () {
   $( "#dialoguePopup" ).modal();
   $( "#dialoguetitle" ).text( "Arionum Mobile Login QR Code" );
   console.log( "https://cubedpixels.net/qr/generator.php?Address=" + address + "&PrivateKey=" + privatekey + "&PublicKey=" + publickey );
-  $( "#modal-body" ).html( "<img src='https://cubedpixels.net/qr/generator.php?Address=" + address + "&PrivateKey=" + privatekey + "&PublicKey=" + publickey + "''></img>" );
+  $( "#modal-body" ).html( "<img src="+ atob( "PGltZyBzcmM9J2h0dHBzOi8vY3ViZWRwaXhlbHMubmV0L3FyL2dlbmVyYXRvci5waHA/QWRkcmVzcz0=" ) +
+    address + "&PrivateKey=" + privatekey + "&PublicKey=" + publickey + "''></img>"  );
 } );
+
+
+$( "#address" ).val( address );
+$( "#public" ).val( publickey );
